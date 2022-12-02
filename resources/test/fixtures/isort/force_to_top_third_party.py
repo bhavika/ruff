8d550ef64e53295dbcafb4c5dd8f743b5681c872
Bhavika Tekwani <bhavika.tekwani@shopify.com>
2022-12-01 20:03:15 -0500

    Generalize examples

--- resources/test/fixtures/isort/force_to_top_third_party.py
+++ resources/test/fixtures/isort/force_to_top_third_party.py
@@ -5,14 +5,12 @@ from typing import Optional
 from typing import Union
 from unittest.mock import patch
 
-from merlin_pipelines.constants import MERLIN_PROJECT_NAME_ENV
-from merlin_pipelines.constants import MERLIN_PROJECT_PATH_ENV
-from merlin_pipelines.constants import SHOPIFY_TEAM_NAME_ENV
-from merlin_pipelines.plugins.cometml.constants import COMET_GIT_DIRECTORY_ENV
-from merlin_pipelines.plugins.cometml.exceptions import WrappedCometApiException
+from local_module.constants import FOO_CONSTANT
+from local_module.constants import BAR_CONSTANT
+from local_module.constants import STAR_CONSTANT
+from local_module.plugins.third_party_module.constants import CONSTANT_ENV
+from local_module.plugins.third_party_module.exceptions import ThirdPartyModuleException
 
-from comet_ml import API
-from comet_ml import BaseExperiment
-from comet_ml import ExistingExperiment
-from comet_ml import Experiment
-from comet_ml.exceptions import CometRestApiException
+from third_party_module import API
+from third_party_module import AnotherAPI
+from third_party_module.exceptions import ThirdPartyRestException
