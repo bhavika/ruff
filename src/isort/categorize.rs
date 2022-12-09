6e45bc978bdef6721b19a1b0d365f51c1a0a3229
Bhavika Tekwani <bhavika.tekwani@shopify.com>
2022-12-09 01:05:18 -0500

    Don't need to categorize force_to_top modules

--- src/isort/categorize.rs
+++ src/isort/categorize.rs
@@ -11,7 +11,7 @@ pub enum ImportType {
     ThirdParty,
     FirstParty,
     LocalFolder,
-    FirstInSection
+    FirstInSection,
 }
 
 pub fn categorize(
@@ -21,7 +21,6 @@ pub fn categorize(
     known_first_party: &BTreeSet<String>,
     known_third_party: &BTreeSet<String>,
     extra_standard_library: &BTreeSet<String>,
-    force_to_top: &BTreeSet<String>,
 ) -> ImportType {
     if level.map_or(false, |level| *level > 0) {
         ImportType::LocalFolder
@@ -37,8 +36,6 @@ pub fn categorize(
         ImportType::StandardLibrary
     } else if find_local(src, module_base) {
         ImportType::FirstParty
-    } else if force_to_top.contains(module_base) {
-        ImportType::FirstInSection
     } else {
         ImportType::ThirdParty
     }
